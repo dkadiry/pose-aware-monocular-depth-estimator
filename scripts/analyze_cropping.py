240cd5bb8f738fc50206c7b8d139f56c7a63bc26
Dami Kadiri <okadiri@uwo.ca>
2024-11-14 21:11:40 -0500

    Initial Manual Analysis Script for Rover Platform

---
+++ scripts/analyze_cropping.py
@@ -0,0 +1,17 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+from utils.tools import get_images_from_directory
+
+def analyze_cropping(images_dir: str, num_samples: int = 100, sample_interval: int = 10):
+    """
+    Loads and displays sample images to determine cropping pixels.
+    
+    Parameters:
+    - images_dir (str): Path to the images directory.
+    - num_samples (int): Number of images to analyze.
+    - sample_interval (int): Interval at which to pause for user input.
+    """
+     
+    pass
+        
