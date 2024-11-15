28ccaa4f13c62dca1e198b36a5a8ba3a49d9837a
Dami Kadiri <okadiri@uwo.ca>
2024-11-14 21:11:40 -0500

    Initial Manual Analysis Script for Rover Platform

--- scripts/analyze_cropping.py
+++ scripts/analyze_cropping.py
@@ -13,8 +13,5 @@ def analyze_cropping(images_dir: str, num_samples: int = 100, sample_interval: i
     - sample_interval (int): Interval at which to pause for user input.
     """
      
-    image_files = get_images_from_directory(images_dir)[:num_samples]
-
-    for idx, image_file in enumerate(image_files):
-        image_path = os.path.join(images_dir, image_file)
-        image = cv2.imread(image_path)
+    pass
+        
