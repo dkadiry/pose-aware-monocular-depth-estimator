c5dcfd277a5640bf68f701f5b99f14708fe83317
Dami Kadiri <okadiri@uwo.ca>
2024-12-06 16:55:15 -0500

    Uncommented lines to start training

--- train.py
+++ train.py
@@ -315,8 +315,7 @@ def main():
 
     # Define callbacks list
     callbacks = [checkpoint_cb, early_stopping_cb, tensorboard_cb]
-    
-    """
+      
     # Train the model
     history = model.fit(
         train_dataset,
@@ -330,11 +329,7 @@ def main():
     os.makedirs(saved_model_dir, exist_ok=True)
     model.save(saved_model_dir)
     print(f"Model saved to {saved_model_dir}")
-    
-    """
-    
-
-       
+           
     
 
 if __name__ == "__main__":
